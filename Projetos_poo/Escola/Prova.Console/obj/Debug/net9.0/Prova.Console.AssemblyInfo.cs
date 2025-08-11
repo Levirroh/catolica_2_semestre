627875f867718bc29ebbc34632e774a868034ae1
= <=>
2025-08-11 19:27:54 -0300

    feat: teste

--- Projetos_poo/Escola/Prova.Console/obj/Debug/net9.0/Prova.Console.AssemblyInfo.cs
+++ Projetos_poo/Escola/Prova.Console/obj/Debug/net9.0/Prova.Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prova.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+352c75c80b3a4d72716fee7beaaf5ff65c0eebb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5863a637926605034021f73d6acb9d70b274a5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prova.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prova.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
