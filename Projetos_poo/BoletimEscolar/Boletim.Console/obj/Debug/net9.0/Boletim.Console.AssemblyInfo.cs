20f2a5a9a8ce93d9fe169e75c3c8e5bcfc27a0a7
= <=>
2025-08-22 21:45:22 -0300

    selection sort

--- Projetos_poo/BoletimEscolar/Boletim.Console/obj/Debug/net9.0/Boletim.Console.AssemblyInfo.cs
+++ Projetos_poo/BoletimEscolar/Boletim.Console/obj/Debug/net9.0/Boletim.Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Boletim.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d034f68b92b36ace1523f82871f3604c37a3ae75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd458c6cc170cb58378b23816c72f861faeb6349")]
 [assembly: System.Reflection.AssemblyProductAttribute("Boletim.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Boletim.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
