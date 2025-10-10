c4aad2470caa3c9371c3263191e5b2eca18c2d86
Levirroh <johann.gr14@gmail.com>
2025-10-10 20:20:16 -0300

    algoritmo de pilha

---
+++ dados_algoritmos/listas/pilha.cpp
@@ -0,0 +1,72 @@
+#include <siostream>
+#include <string>
+
+using namespace std;
+
+struct ELEMENTO{
+	string nome;
+	ELEMENTO *proximo;
+};
+
+main()
+{
+	ELEMENTO *topo, *auxiliar;
+	topo = NULL;
+	auxiliar = NULL;
+	
+	string nome;
+	int op;
+	
+	while (true){
+		system("cls");
+		cout << "---=== Menu ===---" << endl;
+		cout << "1 - Incluir elemento" << endl;
+		cout << "2 - Excluir elemento" << endl;
+		cout << "3 - Mostrar pilha" << endl;
+		cout << "0 - Sair" << endl;
+		cout << "Opcao desejada: ";
+		cin >> op;		
+		
+		if(op == 0){
+			return;
+		}
+		
+		if(op == 1){
+			cout << endl << "---=== Inclusao de elemento ===---" << endl;
+			cout << "Nome para incluir : ";
+			cin >> nome;
+			
+			//cria o novo elemento;
+			auxiliar = new ELEMENTO();
+			auxiliar->nome = nome;
+			auxiliar->proximo = NULL;
+			
+			if(topo == NULL){
+				topo = auxiliar;
+			} else {
+				auxiliar->proximo = topo;
+				topo = auxiliar;
+			}
+		}
+		
+		if(op == 2){
+			if(topo != NULL){
+				auxiliar = topo;
+				topo = topo->proximo;
+				delete auxiliar;
+			}
+		}
+		
+		if(op == 3){
+			cout << endl << "---=== Elementos da pilha ===---" << endl;
+			
+			auxiliar = topo;
+			
+			while (auxiliar->proximo != NULL){
+				cout << auxiliar->nome << endl;
+				auxiliar = auxiliar->proximo;
+			}
+			system("pause");
+		}
+	}
+}
